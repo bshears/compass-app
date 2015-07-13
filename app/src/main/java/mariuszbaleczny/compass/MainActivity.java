a8b4d453a4dcfc2af06f858320d8c3a606c35c65
bshears <mariuszbaleczny@gmail.com>
2015-07-13 20:19:59 +0200

    Classic compass pointing to north

--- app/src/main/java/mariuszbaleczny/compass/MainActivity.java
+++ app/src/main/java/mariuszbaleczny/compass/MainActivity.java
@@ -9,17 +9,27 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
+    private static final float ALPHA = 0.05f;
+    private static final int NUMBER_OF_MEASURMENTS = 5;
     private ImageView mPointer;
     private SensorManager mSensorManager;
-    private Sensor mOrientationSensor;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
     private float mCurrentDegree = 0f;
-    private TextView declinationTextView;
-    private float degree;
+    private int measurementCounter = 0;
+    private ArrayList measurments = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,31 +37,67 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mPointer = (ImageView) findViewById(R.id.arrowPointer);
-        declinationTextView = (TextView) findViewById(R.id.declinationTextView);
     }
 
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mOrientationSensor, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
         Toast.makeText(this, getString(R.string.calibration_info), Toast.LENGTH_SHORT).show();
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this, mOrientationSensor);
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+    }
+
+    private float[] applyLowPassFilter(float[] input, float[] output) {
+        if (output == null) return input;
+
+        for (int i = 0; i < input.length; i++) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        degree = Math.round(event.values[0]);
-        declinationTextView.setText(String.valueOf(degree));
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
 
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+
+            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+            azimuthInDegrees = Math.round(azimuthInDegrees);
+            measurments.add(measurementCounter, azimuthInDegrees);
+
+            if (measurementCounter == NUMBER_OF_MEASURMENTS) {
+                float out = getMeasurmentAverage(NUMBER_OF_MEASURMENTS, measurments);
+                animatePointer(out);
+                mCurrentDegree = -out;
+                measurementCounter = 0;
+            } else {
+                measurementCounter++;
+            }
+        }
+    }
+
+    private void animatePointer(float measurment) {
         RotateAnimation ra = new RotateAnimation(
                 mCurrentDegree,
-                -degree,
+                -measurment,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF,
                 0.5f);
@@ -60,7 +106,15 @@ public class MainActivity extends Activity implements SensorEventListener {
         ra.setFillAfter(true);
 
         mPointer.startAnimation(ra);
-        mCurrentDegree = -degree;
+    }
+
+    private float getMeasurmentAverage(int measurementsNumber, ArrayList arrayData) {
+        float output = 0f;
+
+        for (int i = 0; i < measurementsNumber; i++) {
+            output += (float) arrayData.get(i);
+        }
+        return output / measurementsNumber;
     }
 
     @Override
